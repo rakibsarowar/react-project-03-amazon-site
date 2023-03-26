72e66b9593c0f8832ab2e88cdd93bbb5618da257
rakibsarowar <rakibsarowar@gmail.com>
2023-03-26 21:34:09 +0600

    called api by fatch

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then (data => setProducts(data))
+
+    }, [])
+
+
+
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h2>Product: {products.length}</h2>
+            </div>
+            <div className="card-container">
+                <h4>Product Summery</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
