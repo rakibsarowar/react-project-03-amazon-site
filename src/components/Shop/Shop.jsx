75d95a8b21ca7daf6c21b330dd2707f6c81937be
rakibsarowar <rakibsarowar@gmail.com>
2023-03-26 21:46:10 +0600

    showing product img in card

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then (data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
 
     }, [])
-
-
-
 
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>Product: {products.length}</h2>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                    ></Product>)
+                }
+
+
+
             </div>
+
             <div className="card-container">
                 <h4>Product Summery</h4>
             </div>
