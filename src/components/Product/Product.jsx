14e2165cc2adfc202713eb69c1e7558461996919
rakibsarowar <rakibsarowar@gmail.com>
2023-03-26 22:59:13 +0600

    working in card component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price}=props.product;
+    const {img, name, seller, quantity,ratings, price}=props.product;
     console.log(props.product)
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price: $ {price}</p>
+            <p>Manufacture: {seller}</p>
+            <p>Rating: {ratings} Starts</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
